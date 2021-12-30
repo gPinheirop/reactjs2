713fa6c58035256c21576eddc764875ee35df338
gPinheirop <gabrielpedreira_@hotmail.com>
2021-12-30 12:47:39 -0300

    algumas mudanças estéticas

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,9 @@ function App() {
 
   return (
     <div className="App">
-      <ColorDisplay colorValue={colorValue}/>
+      <ColorDisplay
+       colorValue={colorValue}
+      />
       <ColorInputBar 
         colorValue={colorValue}
         setColorValue={setColorValue}
--- my-app/src/ColorInputBar.jsx
+++ my-app/src/ColorInputBar.jsx
@@ -1,6 +1,6 @@
 const ColorInputBar = ({ colorValue, setColorValue }) => {
     return (
-        <form onSubmit={(e) => e.preventDefault()}>
+        <form  onSubmit={(e) => e.preventDefault()}>
             <label>Add Color Name:</label>
             <input
                 type="text" 
