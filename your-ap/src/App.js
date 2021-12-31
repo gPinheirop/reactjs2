9e917fcb72c1c260978164dd912864de4bf8ad2c
gPinheirop <gabrielpedreira_@hotmail.com>
2021-12-31 12:11:15 -0300

    Criação do banco de dados local e uso do useEffect
    implementação do useEffect dentro do código da lista de compras

--- your-ap/src/App.js
+++ your-ap/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import './App.css';
 import Header from './Header';
@@ -8,32 +8,49 @@ import Content from './Content';
 import Footer from './Footer';
 
 function App() {
+  const API_URL = 'http://localhost:3500/items';
 
-  const[items, setItems] = useState(JSON.parse(localStorage.getItem('shoppingList')));
-
+  const[items, setItems] = useState([])
   const [newItem, setNewItem] = useState('')
-  
   const [search, setSearch] = useState('')
+  const [fetchError, setFetchError] = useState(null)
+  const [isLoading, setIsLoading] = useState(true);
 
-  const setAndSaveitems = ( newItems )=>{
-    setItems(newItems);
-    localStorage.setItem('shoppingList', JSON.stringify(newItems));
-  }
+  useEffect(()=>{
+    const fetchItems = async ()=>{
+      try{
+        const response = await fetch(API_URL);
+        if(!response.ok) throw Error('did not receive expected data');
+        const listItems = await response.json();
+        setItems(listItems);
+        setFetchError(null);
+      }catch (err){
+        setFetchError(err.message);
+      } finally{
+        setIsLoading(false);
+      }
+    }
+    setTimeout(()=> {
+      (async () => await fetchItems())();
+    }, 2000)
+    
+
+  }, [])
 
   const addItem =(item) =>{
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = {id, checked : false, item };
     const listItems = [...items, myNewItem];
-    setAndSaveitems(listItems);
+    setItems(listItems);
   }
 
   const handleCheck = (id)=>{
     const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item);
-    setAndSaveitems(listItems);
+    setItems(listItems);
   }
   const handleDelete = (id)=>{
     const listItems = items.filter((item) => item.id!== id) 
-    setAndSaveitems(listItems);
+    setItems(listItems);
   }
 
   const handleSubmit = (e) =>{
@@ -55,11 +72,15 @@ function App() {
         search={search}
         setSearch={setSearch}
       />
-      <Content 
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()) )}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
+      <main>
+        {isLoading && <p>Loading Items...</p>}
+        {fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading && <Content 
+          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()) )}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />}
+      </main>
       <Footer length ={items.length} />
     </div>
   );
